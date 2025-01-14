22fd542dabf563b004af78af925820fa59b9941b
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-14 10:27:00 -0500

    feat(servicios): Agregar css a Description
    Mobile first usando tailwind

--- app/(client)/servicios/components/Description.jsx
+++ app/(client)/servicios/components/Description.jsx
@@ -1,8 +1,8 @@
 export default function Description({ title, text }) {
   return (
-    <section>
-      <h3>{title}</h3>
-      <p>{text}</p>
+    <section className="bg-[#973cd1] text-white p-4">
+      <h3 className="font-bold text-xl mb-2">{title}</h3>
+      <p className="text-justify">{text}</p>
     </section>
   );
 }
