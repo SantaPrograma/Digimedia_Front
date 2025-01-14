9953768c68e980f94247b0ac67e3f10aa65cfac5
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-14 11:18:54 -0500

    feat(servicios): Agregar css a Contactanos
    Mobile first usando tailwind

--- app/(client)/servicios/components/Contactanos.jsx
+++ app/(client)/servicios/components/Contactanos.jsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 
 export default function Contactanos({ text, iconLeft, iconRight }) {
   return (
-    <section>
-      <p>{text}</p>
-      <a>Contáctanos ahora</a>
-      <Image src={iconLeft} alt="" />
-      <Image src={iconRight} alt="" />
+    <section className="bg-[#7b22b3] text-white font-bold text-4xl text-center p-12 relative">
+      <p className="mb-8 z-10 relative">{text}</p>
+      <a className="bg-[#ff037f] p-4 inline-block rounded-2xl z-10 relative">
+        Contáctanos ahora
+      </a>
+      <Image className="absolute bottom-4 left-0" src={iconLeft} alt="" />
+      <Image className="absolute bottom-4 right-0" src={iconRight} alt="" />
     </section>
   );
 }
