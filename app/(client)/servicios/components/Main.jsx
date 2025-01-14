1e25b7cd04427821dbf05a5cacb2ca511a12c2a4
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-14 10:09:46 -0500

    feat(servicios): Agregar css a Main
    Mobile first usando tailwind

--- app/(client)/servicios/components/Main.jsx
+++ app/(client)/servicios/components/Main.jsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 
 export default function Main({ title, subtitle, text, image }) {
   return (
-    <main>
-      <div>
-        <h1>{title}</h1>
-        <h2>{subtitle}</h2>
+    <main className="flex flex-col-reverse items-center">
+      <div className=" text-center">
+        <h1 className="text-[#7b22b3] font-bold text-5xl m-4">{title}</h1>
+        <h2 className="font-bold text-2xl m-2">{subtitle}</h2>
         <p>{text}</p>
-        <a>¿CÓMO TRABAJAMOS?</a>
+        <a className="bg-[#7b22b3] text-white font-bold p-3 inline-block rounded-full m-8">
+          ¿CÓMO TRABAJAMOS?
+        </a>
       </div>
       <Image src={image} alt="" />
     </main>
