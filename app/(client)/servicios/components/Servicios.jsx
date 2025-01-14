0f1310a4daa9b4d90807fd365dc722181bcb97ca
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-14 13:05:45 -0500

    feat(servicios): Volver responsive a Servicios

--- app/(client)/servicios/components/Servicios.jsx
+++ app/(client)/servicios/components/Servicios.jsx
@@ -13,19 +13,19 @@ export default function Servicios({ servicios }) {
   });
 
   return (
-    <section className="p-4">
+    <section className="p-4 max-w-6xl m-auto">
       <h3 className="font-bold text-xl mb-2">Nuestros Servicios</h3>
-      {serviciosList}
+      <div className="flex flex-wrap justify-center gap-4">{serviciosList}</div>
     </section>
   );
 }
 
 function Servicio({ title, text, icon }) {
   return (
-    <div className="bg-[#973cd1] text-white text-center flex flex-col items-center gap-4 p-4 my-4">
-      <Image className="max-w-52" src={icon} alt="" />
+    <div className="bg-[#973cd1] text-white text-center flex flex-col items-center justify-between gap-4 p-4 basis-64 flex-1 shrink-0 min-[832px]:max-[1118px]:basis-96">
+      <Image className="max-w-36" src={icon} alt="" />
       <h4 className="font-bold text-2xl max-w-52">{title}</h4>
-      <p className="max-w-52">{text}</p>
+      <p className="max-w-48 basis-24">{text}</p>
       <a className="bg-white text-[#973cd1] font-bold px-8 py-2 rounded-full">
         CONOCE MÁS
       </a>
