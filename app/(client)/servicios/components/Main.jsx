dc5d93c37b5672154eb2e130acd8a9c5423caee5
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-14 13:45:55 -0500

    feat(servicios): Volver responsive a Main

--- app/(client)/servicios/components/Main.jsx
+++ app/(client)/servicios/components/Main.jsx
@@ -2,16 +2,18 @@ import Image from 'next/image';
 
 export default function Main({ title, subtitle, text, image }) {
   return (
-    <main className="flex flex-col-reverse items-center">
-      <div className=" text-center">
-        <h1 className="text-[#7b22b3] font-bold text-5xl m-4">{title}</h1>
-        <h2 className="font-bold text-2xl m-2">{subtitle}</h2>
+    <main className="flex flex-col-reverse items-center justify-between max-w-6xl m-auto md:flex-row p-4 md:py-16">
+      <div className=" text-center md:text-left max-w-xl">
+        <h1 className="text-[#7b22b3] font-bold text-5xl my-4 font-title">
+          {title}
+        </h1>
+        <h2 className="font-bold text-2xl my-2">{subtitle}</h2>
         <p>{text}</p>
-        <a className="bg-[#7b22b3] text-white font-bold p-3 inline-block rounded-full m-8">
+        <a className="bg-[#7b22b3] text-white font-bold p-3 inline-block rounded-full mt-8">
           ¿CÓMO TRABAJAMOS?
         </a>
       </div>
-      <Image src={image} alt="" />
+      <Image className="max-w-96 w-full" src={image} alt="" />
     </main>
   );
 }
