ce47dd79eb39136f474e988ab7eb49393a2bdeaf
JerryRivera609 <jerry609rivera@gmail.com>
2025-01-15 22:12:02 -0500

    Sección testimonios culminada

--- app/(client)/Home_Components/testimonios.jsx
+++ app/(client)/Home_Components/testimonios.jsx
@@ -1,16 +1,18 @@
 import Image from 'next/image';
+import './testimonios.css'
 import opinion from '@/public/image-home/OPINIONES.jpeg'
 
 function testimonios(){
     return(
-        <section className="row stretch mx-0 d-fle p-3x aos-init aos-animate" data-aos="fade-up">
-            <div className="col-12 col-md-6 box-opiniones px-2 py-3 px-md-5 py-md-5 text-center text-md-start">
-                <span className="text-4">TESTIMONIOS</span>
-                <h5 className="text-5">NUESTROS CLIENTES</h5>
-                <h1 className="text-6 fw-bold">OPINAN</h1>
-            </div>
-            <div className="col-12 col-md-6 px-0 d-flex">
-                <Image src={opinion} width="100%" height="auto" />
+        <section id='testimonios'>
+            <div className="testimonios-main">
+                <div className="testimonios-child testimonios-text">
+                    <h3>Testimonios</h3>
+                    <h3>NUESTROS CLIENTES <span>OPINAN</span></h3>
+                </div>
+                <div className="testimonios-child testimonios-img">
+                    <Image src={opinion} alt="" />
+                </div>
             </div>
         </section>
     );
