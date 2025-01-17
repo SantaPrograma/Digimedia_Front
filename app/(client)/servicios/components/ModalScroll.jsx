6d083e073a69be335cfcb626e9a202d73d2882db
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-17 13:39:48 -0500

    feat(servicios): Añadir estilo al ModalScroll

--- app/(client)/servicios/components/ModalScroll.jsx
+++ app/(client)/servicios/components/ModalScroll.jsx
@@ -5,26 +5,38 @@ import logo from '@/public/servicios/logo-modal.webp';
 export default function ModalScroll({ text, fondo, title }) {
   return (
     <div className="bg-[rgb(0,0,0,0.5)] w-screen h-screen flex items-center justify-center fixed top-0 left-0 z-50">
-      <div className="bg-white">
-        <button>X</button>
-        <div>
-          <Image src={fondo} alt="" />
-          <Image src={logo} alt="" />
-          <p>{text}</p>
+      <div className="bg-black flex relative text-white rounded-2xl overflow-hidden">
+        <button className="absolute top-4 right-4">X</button>
+        <div className="relative">
+          <Image className="h-full" src={fondo} alt="" />
+          <Image className="absolute top-4 left-4" src={logo} alt="" />
+          <p className="absolute bottom-10 right-6 text-3xl font-semibold text-right">
+            {text}
+          </p>
         </div>
-        <div>
-          <p>{title}</p>
-          <form>
-            <label htmlFor="name">Nombre</label>
-            <input id="name" type="text" />
-            <label htmlFor="phone">Teléfono</label>
-            <input id="phone" type="text" />
-            <label htmlFor="email">Correo</label>
-            <input id="email" type="email" />
-            <button>HAZLO YA</button>
+        <div className="p-8 flex flex-col justify-between w-96 gap-8 bg-gradient-to-b from-[#0095ff] to-[#ff037f]">
+          <p className="text-3xl text-center font-bold">{title}</p>
+          <form className="flex flex-col gap-4">
+            <Input label="Nombre" type="text" name="name" />
+            <Input label="Teléfono" type="text" name="phone" />
+            <Input label="Correo" type="email" name="email" />
+            <button className="bg-[#0095ff] p-2 text-2xl font-bold rounded-2xl mt-4">
+              HAZLO YA
+            </button>
           </form>
         </div>
       </div>
+    </div>
+  );
+}
+
+function Input({ label, type, name }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <label className="font-semibold" htmlFor={name}>
+        {label}
+      </label>
+      <input className="p-2 rounded-md" id={name} name={name} type={type} />
     </div>
   );
 }
