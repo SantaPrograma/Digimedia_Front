68be47849fb008909f29095b383ba60aecd387ca
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-22 13:53:23 -0500

    fix(servicios): Cambiar Image a img

--- app/(client)/servicios/components/Contactanos.jsx
+++ app/(client)/servicios/components/Contactanos.jsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export default function Contactanos({ text, iconLeft, iconRight }) {
   return (
     <section className="bg-[#7b22b3] text-white font-bold text-3xl text-center p-12 relative">
@@ -10,8 +8,8 @@ export default function Contactanos({ text, iconLeft, iconRight }) {
       >
         Contáctanos ahora
       </button>
-      <Image className="absolute bottom-4 left-0" src={iconLeft} alt="" />
-      <Image className="absolute bottom-4 right-0" src={iconRight} alt="" />
+      <img className="absolute bottom-4 left-0" src={iconLeft} alt="" />
+      <img className="absolute bottom-4 right-0" src={iconRight} alt="" />
     </section>
   );
 }
