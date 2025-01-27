9c0ea561df23e86b52a31a5453b8b6b395a746d7
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-27 13:12:46 -0500

    feat(dashboard): Agregar estilo a Table

--- app/dashboard/components/Table.jsx
+++ app/dashboard/components/Table.jsx
@@ -1,22 +1,30 @@
 export default function Table({ headers, data }) {
   const headersList = headers.map((header) => {
-    return <th>{header.toUpperCase()}</th>;
+    return <th className="p-2">{header.toUpperCase()}</th>;
   });
 
   const dataList = data.map((dataRow) => {
     const row = [];
 
     for (let i = 0; i < headers.length; i++) {
-      row.push(<td>{dataRow[headers[i]]}</td>);
+      row.push(<td className="p-2">{dataRow[headers[i]]}</td>);
     }
     row.push(
       <td>
-        <button>Eliminar</button>
-        <button>Editar</button>
+        <button className="bg-[#dc3545] text-white rounded-md px-3 py-2 m-2">
+          Eliminar
+        </button>
+        <button className="bg-[#ffc107] rounded-md px-3 py-2 m-2">
+          Editar
+        </button>
       </td>
     );
 
-    return <tr key={dataRow.id}>{row}</tr>;
+    return (
+      <tr key={dataRow.id} className="odd:bg-[#f2f2f2]">
+        {row}
+      </tr>
+    );
   });
 
   return (
