16e9ecf9ac46dcf34f61cb14be3a9e16ead33d20
Emanuel Serveleon <serveleonemanuel@gmail.com>
2025-01-28 11:50:57 -0500

    fix(dashboard): agregar keys faltantes

--- app/dashboard/components/Table.jsx
+++ app/dashboard/components/Table.jsx
@@ -1,16 +1,24 @@
 export default function Table({ headers, data }) {
-  const headersList = headers.map((header) => {
-    return <th className="p-2">{header.toUpperCase()}</th>;
+  const headersList = headers.map((header, index) => {
+    return (
+      <th key={index} className="p-2">
+        {header.toUpperCase()}
+      </th>
+    );
   });
 
   const dataList = data.map((dataRow) => {
     const row = [];
 
     for (let i = 0; i < headers.length; i++) {
-      row.push(<td className="p-2">{dataRow[headers[i]]}</td>);
+      row.push(
+        <td key={i} className="p-2">
+          {dataRow[headers[i]]}
+        </td>
+      );
     }
     row.push(
-      <td>
+      <td key={50}>
         <button className="bg-[#dc3545] text-white rounded-md px-3 py-2 m-2">
           Eliminar
         </button>
