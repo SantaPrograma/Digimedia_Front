16d83137163e5b75cc358672cdefee1ba2cfbd4f
JefersonDominguez <jeffersondominguez082@gmail.com>
2025-01-29 11:11:34 -0500

    pregunta visible con clic

--- app/(client)/preguntas/components/Pregunta.jsx
+++ app/(client)/preguntas/components/Pregunta.jsx
@@ -1,11 +1,18 @@
-export default function Pregunta({question, answer}) {
+"use client";
+import { useState } from "react";
 
-    return (
-        <>
-            <article  className="border-l-4 pl-4 border-solid border-[#5c1787]">
-                <h3 className="font-bold text-[25px]">{question}</h3>
-                <p className="text-[18px] mt-4">{answer}</p>
-            </article>
-        </>
-    )
+export default function Pregunta({ question, answer }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <article className="border-l-4 pl-4 border-solid border-[#5c1787]">
+      <h3 
+        className="font-bold text-[25px] cursor-pointer hover:text-[#5c1787]" 
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {question}
+      </h3>
+      {isOpen && <p className="text-[18px] mt-4">{answer}</p>}
+    </article>
+  );
 }
